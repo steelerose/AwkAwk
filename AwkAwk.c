278fb1b117ed8893f1d9d2aec538bfeecd9bf181
Julie <julie.d.rose@gmail.com>
2014-05-04 17:03:25 -0700

    initial commit

---
+++ AwkAwk.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char input[500];
+
+  fgets(input, 499, stdin);
+
+  printf("Input: %s\n", input);
+
+  if (strlen(input) == 0) {
+    printf("Invalid input: File is empty.\n");
+  } else if (!strstr(input, "CPU")) { //need to check lower & mixed case too
+    printf("Invalid input: No reference to CPU\n");
+  } else {
+
+    int count = 0;
+    int i = 0;
+    while (i < strlen(input)) {
+      if (strstr(input, "CPU")) {
+        count++;
+        *strstr(input, "CPU") = 'x';
+      }
+
+      i = i + 3;
+    }
+
+    printf("CPU count: %d\n", count);
+  }
+
+  return 0;
+}
